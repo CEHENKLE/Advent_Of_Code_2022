65c45995076fa9400a24730df686e52157923921
CEHENKLE <henkle@amazon.com>
2022-12-08 19:43:47 -0800

    Day 2, part 2 who the hell lists things in 'Lose, Draw, Win' order?!??

--- Day_2.py
+++ Day_2.py
@@ -1,4 +1,6 @@
 def winnerwinner(him, you):
+    print('winner')
+    print(him, you)
     if (him == 'A'):
         if you == 'X':
             return 4 # Rock (1) plus Draw (3)
@@ -21,16 +23,57 @@ def winnerwinner(him, you):
         else:
             return 6 # Scissors (3) plus draw (3)
 
+def blacksocks(him,outcome):
+    print('blacksocks')
+    print(him,outcome)
+    if outcome == 'X': #lose
+        if him == 'A':
+            return winnerwinner(him, 'Z')
+        if him == 'B':
+            return winnerwinner(him, 'X')
+        else:
+            return winnerwinner(him, 'Y')
+    if outcome == 'Y': # Draw
+        if him == 'A':
+            return winnerwinner(him, 'X')
+        if him == 'B':
+            return winnerwinner(him, 'Y')
+        else:
+            return winnerwinner(him,'Z')
+    else: #win
+        if him == "A":
+            return winnerwinner(him,'Y')
+        if him == "B":
+            return winnerwinner(him,'Z')
+        else:
+            return winnerwinner(him, 'X')
+
 #testing
 #print(winnerwinner('A','Y'))
 #print(winnerwinner('B','X'))
 #print(winnerwinner('C','Z'))
 
+
+# part 1
 total = 0
 with open('Day_2_AOC.txt') as file:
     for lines in file:
         him, me = [(x) for x in lines.split()]
-        print(him,me)
+#        print(him,me)
         total+=(winnerwinner(him, me))
-        print(total)
+#        print(total)
+print(total)
+
+#testing
+#print(blacksocks('A','Y'))
+#print(blacksocks('B','X'))
+#print(blacksocks('C','Z'))
+#print('--------------------------------------------------------')
+
+#Part 2
+total=0
+with open('Day_2_AOC.txt') as file:
+    for lines in file:
+        him, outcome = [(x) for x in lines.split()]
+        total += (blacksocks(him, outcome))
 print(total)
